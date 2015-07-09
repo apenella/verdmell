a498681ea3071bb78f6edb0017c7f6201f151f3f
Aleix Penella <aleix.penella@gmail.com>
2015-07-09 15:01:49 +0200

    ui: add check and sample information for a service check

--- ui/scripts/scripts.js
+++ ui/scripts/scripts.js
@@ -3,9 +3,11 @@
 	generateServicesDetails = 
 
 	$(document).ready(function(){
+
 		$(".clusternode").click(function () {
-			var url = $(this).attr('url')+"/api/services"
-			$.getJSON(url, function(services) {
+			var baseUrl = $(this).attr('url')
+			var serviceUrl = $(this).attr('url')+"/api/services"
+			$.getJSON(serviceUrl, function(services) {
 				var servicesinfo = '<div class="servicesdetail">';
 				$.each(services.services, function(key, service) {
 					servicesinfo += '<div id="'+service.name+'" class="servicedetail">';
@@ -14,17 +16,54 @@
 						servicesinfo += '</div>';					
 						servicesinfo += '<div class="servicechecks">';
 							for ( c in service.checks) {
-								servicesinfo += '<div class="servicecheck">'+service.checks[c]+'</div>'
+								servicesinfo += '<div class="servicecheck" url="'+baseUrl+'" name="'+service.checks[c]+'">'+service.checks[c]+'</div>'
 							}
+						servicesinfo += '<div style="clear:both;"></div>';
+						servicesinfo += '<div class="servicecheckdetails"></div>';
+						servicesinfo += '<div class="servicechecksampledetails"></div>';
+						servicesinfo += '<div style="clear:both;"></div>';
 						servicesinfo += '</div>';
 					servicesinfo += '</div>';			
 				});
 				servicesinfo += '</div>';
-				
-				$(".clusternodedetails").html(servicesinfo);
-			});
 
+				$(".clusternodedetails").html(servicesinfo);
+				setActionToServicesChecks();
+			//end getJSON
+			});
+		//end click
 		});
 
+
 	});
+
+	function setActionToServicesChecks () {
+		$(".servicecheck").click(function(){
+			var baseUrl = $(this).attr('url');
+			var check = $(this).attr('name');
+			var checkUrl = baseUrl+"/api/checks/"+check;
+			var sampleUrl = baseUrl+"/api/samples/"+check;
+
+			$.getJSON(checkUrl, function(check) {
+				var checkinfo = '<div class="checkinfo" id="checkname">'+check.name+'</div>';
+				checkinfo += '<div class="checkinfo" id="checkdescription">'+check.description+'</div>';
+				checkinfo += '<div class="checkinfo" id="checkcommand">'+check.command+'</div>';
+				$(".servicecheckdetails").html(checkinfo);
+			//end getJSON
+			});
+
+			$.getJSON(sampleUrl, function(sample) {
+				var sampleinfo = '<div class="sampleinfo" id="sampletime">'+sample.sampletime+'</div>';
+				sampleinfo += '<div class="sampleinfo" id="elapsedtime">'+sample.elapsedtime+'</div>';
+				sampleinfo += '<div class="sampleinfo" id="expirationtime">'+sample.expirationtime+'</div>';
+				sampleinfo += '<div class="sampleinfo" id="output">'+sample.output+'</div>';
+				sampleinfo += '<div class="sampleinfo" id="timestamp">'+sample.timestamp+'</div>';
+				$(".servicechecksampledetails").html(sampleinfo);
+			//end getJSON
+			});
+
+		//end click
+		});
+	}
+
 {{end}}
